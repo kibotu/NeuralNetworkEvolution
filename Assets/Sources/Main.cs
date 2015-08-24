2f081bd79a2ce60b564b14d3472cf09fd4bb1b09
Jan Rabe <janrabe@kibotu.net>
2015-08-25 00:46:50 +0200

    init

---
+++ Assets/Sources/Main.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Sources
+{
+    public class Main : MonoBehaviour {
+
+        void Start () {
+	        
+        }
+	
+        void Update () {
+	
+        }
+    }
+}
